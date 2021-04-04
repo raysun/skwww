a123d5323af705f4b9c4cf1098d3681fef388ec0
Ray Sun <raysun@gmail.com>
2021-04-03 17:19:29 -0700

    Revert protected all the way back to the original source

--- pages/protected.js
+++ pages/protected.js
@@ -2,32 +2,10 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/client";
 import Layout from "../components/layout";
 import AccessDenied from "../components/access-denied";
-// import styles from "../styles/Home.module.css";
-// import useSWR from "swr";
-// import fetcher from "../components/Fetcher";
 
 export default function Page() {
   const [session, loading] = useSession();
   const [content, setContent] = useState();
-
-  // const apiURL = `/api/examples/protected`;
-  // const { data, error } = useSWR(apiURL, fetcher, {
-  //   revalidateOnFocus: false,
-  // });
-  // if (error) return <div>Error</div>;
-  // if (!data)
-  //   return (
-  //     <Layout>
-  //       <div className={styles.container}>
-  //         <main className={styles.main}>
-  //           <h1 className={styles.title}>Clash Sidekick</h1>
-  //           <h2>Loading...</h2>
-  //         </main>
-  //       </div>
-  //     </Layout>
-  //   );
-  // console.log(data);
-  // console.log(session);
 
   // Fetch content from protected route
   useEffect(() => {
