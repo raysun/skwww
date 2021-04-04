82490818c4936c89ce541582c5864b43399d1ecb
Ray Sun <raysun@gmail.com>
2021-04-03 17:17:10 -0700

    Revert protected to use the sample code instead of SWT for now

--- pages/protected.js
+++ pages/protected.js
@@ -2,47 +2,47 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/client";
 import Layout from "../components/layout";
 import AccessDenied from "../components/access-denied";
-import styles from "../styles/Home.module.css";
-import useSWR from "swr";
-import fetcher from "../components/Fetcher";
+// import styles from "../styles/Home.module.css";
+// import useSWR from "swr";
+// import fetcher from "../components/Fetcher";
 
 export default function Page() {
   const [session, loading] = useSession();
-  // const [content, setContent] = useState();
+  const [content, setContent] = useState();
 
-  const apiURL = `/api/examples/protected`;
-  const { data, error } = useSWR(apiURL, fetcher, {
-    revalidateOnFocus: false,
-  });
-  if (error) return <div>Error</div>;
-  if (!data)
-    return (
-      <Layout>
-        <div className={styles.container}>
-          <main className={styles.main}>
-            <h1 className={styles.title}>Clash Sidekick</h1>
-            <h2>Loading...</h2>
-          </main>
-        </div>
-      </Layout>
-    );
-  console.log(data);
-  console.log(session);
+  // const apiURL = `/api/examples/protected`;
+  // const { data, error } = useSWR(apiURL, fetcher, {
+  //   revalidateOnFocus: false,
+  // });
+  // if (error) return <div>Error</div>;
+  // if (!data)
+  //   return (
+  //     <Layout>
+  //       <div className={styles.container}>
+  //         <main className={styles.main}>
+  //           <h1 className={styles.title}>Clash Sidekick</h1>
+  //           <h2>Loading...</h2>
+  //         </main>
+  //       </div>
+  //     </Layout>
+  //   );
+  // console.log(data);
+  // console.log(session);
 
   // Fetch content from protected route
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch("/api/examples/protected");
-  //     const json = await res.json();
-  //     if (json.content) {
-  //       setContent(json.content);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [session]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/examples/protected");
+      const json = await res.json();
+      if (json.content) {
+        setContent(json.content);
+      }
+    };
+    fetchData();
+  }, [session]);
 
   // When rendering client side don't display anything until loading is complete
-  // if (typeof window !== "undefined" && loading) return null;
+  if (typeof window !== "undefined" && loading) return null;
 
   // If no session exists, display access denied message
   if (!session) {
@@ -58,7 +58,7 @@ export default function Page() {
     <Layout>
       <h1>Protected Page</h1>
       <p>
-        <strong>{data || "\u00a0"}</strong>
+        <strong>{content || "\u00a0"}</strong>
       </p>
       <p>{session}</p>
     </Layout>
