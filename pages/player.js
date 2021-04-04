6d3dceecc4c21f298182bf3a78175838b0ce3f68
Ray Sun <raysun@gmail.com>
2021-04-03 18:22:48 -0700

    prevent loading when players is null

--- pages/player.js
+++ pages/player.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/client";
 import Layout from "../components/layout";
 import AccessDenied from "../components/access-denied";
-import useSWR from "swr";
+// import useSWR from "swr";
 // import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
@@ -26,7 +26,7 @@ export default function Page() {
   if (typeof window !== "undefined" && loading) return null;
 
   // If no session exists, display access denied message
-  if (!session) {
+  if (!session || players.length == 0) {
     return (
       <Layout>
         <AccessDenied />
