82d395760749534f3d4e1a88b94ef1d984a5f606
Ray Sun <raysun@gmail.com>
2021-04-03 18:01:08 -0700

    Fix some bugs in protected

--- pages/protected.js
+++ pages/protected.js
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/client";
 import Layout from "../components/layout";
 import AccessDenied from "../components/access-denied";
 import useSWR from "swr";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 export default function Page() {
@@ -34,11 +34,10 @@ export default function Page() {
     );
   }
 
-  const router = useRouter();
-
-  const tag = router.query.tag;
-  const apiURL = `/api/clan?tag=${tag}`;
-  console.log(apiURL);
+  // const router = useRouter();
+  // const tag = router.query.tag;
+  const apiURL = `/api/clan`;
+  // console.log(apiURL);
   const { data, error } = useSWR(apiURL, fetcher, {
     revalidateOnFocus: false,
   });
