c81bad7ee1216a01e06b1209a24da9a6ec80f5eb
Ray Sun <raysun@gmail.com>
2021-04-05 19:17:18 -0700

    default to discord sign-in

--- components/header.js
+++ components/header.js
@@ -29,7 +29,7 @@ export default function Header() {
                 className={styles.buttonPrimary}
                 onClick={(e) => {
                   e.preventDefault();
-                  signIn();
+                  signIn("discord");
                 }}
               >
                 Sign in
