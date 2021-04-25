42b96f8935b5ea5f9fbdc687ff0d444b0b2d55f0
Ray Sun <raysun@gmail.com>
2021-04-24 17:34:18 -0700

    Replace the Grid header with a standard Toolbar

--- components/header.js
+++ components/header.js
@@ -10,25 +10,61 @@ import {
   GridList,
   GridListTile,
   GridListTileBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 // The approach used in this component shows how to built a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
+  const classes = useStyles();
   const [session, loading] = useSession();
 
   return (
+    <Toolbar>
+      <Typography variant="h5" className={classes.title}>
+        Clash Sidekick
+      </Typography>
+      {session && session.user.image && (
+        <Avatar className={styles.avatar} src={session.user.image}></Avatar>
+      )}
+      <strong>{session && session.user.name}</strong>
+      <Button
+        color="inherit"
+        href={`/api/auth/signin`}
+        className={styles.buttonPrimary}
+        onClick={(e) => {
+          e.preventDefault();
+          session ? signOut() : signIn("discord");
+        }}
+      >
+        {session ? "Logout" : "Login"}
+      </Button>
+    </Toolbar>
+  );
+  return (
     <header>
-      <noscript>
-        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-      </noscript>
       <p
         className={`nojs-show ${
           !session && loading ? styles.loading : styles.loaded
         }`}
       >
-        <GridList container spacing="4">
+        <Grid container>
           <Grid className={styles.title} item>
             Clash Sidekick
           </Grid>
@@ -76,7 +112,7 @@ export default function Header() {
               </Grid>
             </Grid>
           )}
-        </GridList>
+        </Grid>
       </p>
       <nav>
         <ul className={styles.navItems}>
